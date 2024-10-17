c74da43563efd77c8fcc3ef3f4d76a643eb18d8c
Gabriel <gaj.gonzalez@duocuc.cl>
2024-10-17 03:06:21 -0300

    Funcion QR

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -15,6 +15,7 @@ import { CamaraQrComponent } from './camara-qr/camara-qr.component';
 import { PerfilComponent } from './perfil/perfil.component';
 import { RegistrarComponent } from './registrar/registrar.component';
 import { CierreSesionComponent } from './cierre-sesion/cierre-sesion.component';
+import { QRCodeModule } from 'angularx-qrcode';
 
 
 @NgModule({
@@ -37,7 +38,8 @@ import { CierreSesionComponent } from './cierre-sesion/cierre-sesion.component';
     FormsModule,
     RouterLink,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    QRCodeModule
   ]
 })
 export class PagesModule { }
